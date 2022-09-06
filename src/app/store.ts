65b4e7664d599a18083666a7fcd14b1ef32ec6cb
theander <aclaudino@gmail.com>
2022-09-05 23:47:53 -0300

    Added dependencies to project

---
+++ src/app/store.ts
@@ -0,0 +1,11 @@
+import{configureStore} from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer:{
+  //      posts:postsReducer,
+   //     comments: commentsReducer,
+  //      users:usersReducer
+    }
+})
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch=typeof store.dispatch
