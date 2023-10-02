bc9c4996e2986d1e166526e5adae0d4ceca00144
theander <aclaudino@gmail.com>
2023-10-01 23:19:46 -0300

    Renomeado variaveis de ambientes

--- src/pages/labs/[id]/concluir.tsx
+++ src/pages/labs/[id]/concluir.tsx
@@ -1,10 +1,11 @@
 import { useRouter } from 'next/router';
 import axios from 'axios';
+import { LABS_API_BASE_URL } from '../../../../components/const/url-constants';
 
 export default function Concluir() {
   const route = useRouter();
   const id = route.query.id;
-  axios.put(`http://localhost:8380/api/lab/${id}/done`).then((resp) => {
+  axios.put(`${LABS_API_BASE_URL}/api/lab/${id}/done`).then((resp) => {
     route.push('/labs/para-fazer');
   });
 }
