3d3d851e59a61c61ccac20a7c46fdbcb311210fd
theander <aclaudino@gmail.com>
2023-10-22 16:01:44 -0300

    Login credential + google implemented

--- components/const/url-constants.ts
+++ components/const/url-constants.ts
@@ -1,6 +1,6 @@
 export const USER_API_BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL_USERS_API1 || 'http://localhost:8180'; //'http://54.80.8.44:8180';
+  process.env.NEXT_PUBLIC_BASE_URL_USERS_API1 || 'http://54.80.8.44:8180';
 export const MEDICAL_API_BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL_MEDICAL_API1 || 'http://localhost:8280';
+  process.env.NEXT_PUBLIC_BASE_URL_MEDICAL_API1 || 'http://54.80.8.44:8280';
 export const LABS_API_BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL_LABS_API1 || 'http://localhost:8380';
+  process.env.NEXT_PUBLIC_BASE_URL_LABS_API1 || 'http://54.80.8.44:8380';
