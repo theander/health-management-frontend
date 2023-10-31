04847f59575a798116901f1158925cf3a18465dd
theander <aclaudino@gmail.com>
2023-10-30 23:47:42 -0300

    removido acesso OAuth sem registro previo

---
+++ src/pages/home/not-registered.tsx
@@ -0,0 +1,15 @@
+export default function NotRegitered() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <h1>Não registrdo</h1>
+      </div>
+      <div className='row'>
+        <p>
+          Obs.: O usuário deve estar registrado préviamente para acessar
+          aplicação utilizando OAuth2
+        </p>
+      </div>
+    </div>
+  );
+}
